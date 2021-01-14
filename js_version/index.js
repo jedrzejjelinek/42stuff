b5eeba40adc56e1909b9ab81447821e509135717
Jedrzej Jelinek <jedrzej.jelinek@gemius.com>
2021-01-14 09:15:04 +0100

    Add package, index and TODO notes

---
+++ js_version/index.js
@@ -0,0 +1,36 @@
+console.log("let's begin");
+
+// TODO notes
+// add karma, jasmine and stuff
+
+// what about reading data: use attached examples
+// 1 first version: load from memory (add reading files later)
+// 2 add loading from file
+// 3 test memory usage and execution time (42tech loads huge data files probably)
+// should delimiter be configurable?
+
+// what about parsing
+// 1 first: assume that are 2 props only
+// 2 how to parse (string comparison for headers? regex?) - test with 200 headers (probably it doesnt matter)
+// 3 what data structure?
+
+// !!! sort function
+// 1 first: code simplest thing with hardcoded net_sales metric used in sorting
+// 2 add configurable metrics
+// 3 (extra) add possibility to configure metric per property
+
+// display result
+// 1 first: just return value - for testing purposes
+// 2 print on screen
+// 3 save to file
+// ...or maybe just save to file, think that throug
+
+// what about tests
+// add unit tests
+// kind of integrational tests using given examples
+// stress tests - ? - prepare large file (maybe based on given examples?)
+// what about corner cases? - think that through
+
+// what about performance?
+// fist: dont worry bout the perf, just make working algorythm
+// second: try to optimize - think that through
